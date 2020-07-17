3ee5ff405325ea52827d216ed65e8c1af2fc8de6
Gilles Fischer <gilles.fischer@sorbonne-universite.fr>
2020-07-17 10:00:13 +0200

    correct naming error 2

--- src/python_script/OLD-00_analyze_telom_length-v2.py
+++ src/python_script/OLD-00_analyze_telom_length-v2.py
@@ -9,12 +9,13 @@ import os.path
 
 ## define the genme fasta file
 if len(sys.argv) != 2:
-    sys.exit("ERROR: you must specify the name of genome fasta file or its path as the first argument")
+    print("ERROR: you must specify the name of genome fasta file or its path as the first argument")
+    sys.exit()
 
 ## function#1: return the strain name, each chromosome name and their telomere length on each side
 def chr_start_end(genome_fasta):
 
-    ## get strain name from fasta files either from a path or in the src directory
+    ## get strain name from fasta files either from a path or in tne src directory
     ## from a path
     if "/" in genome_fasta:
     	file_name = genome_fasta.split("/")
@@ -114,10 +115,8 @@ def chr_start_end(genome_fasta):
             if file_exists:
                 filout.write("{0}\t{1}\tL\t{2}\t{4}\n{0}\t{1}\tR\t{3}\t{5}\n".format(strain, chrom, left_tel, right_tel, offset, revoffset))
             else:
-                filout.write("{0}\t{1}\t{2}\t{3}\t{4}\n{5}\t{6}\tL\t{7}\t{9}\n{5}\t{6}\tR\t{8}\t{10}\n"\
-                    .format("Strain", "Chromosome", "Contig_side", "Telom_length", "Offset", \
-                    strain, chrom, left_tel, right_tel, offset, revoffset)) # file doesn't exist yet, write a header
+                filout.write("{0}\t{1}\t{2}\t{3}\t{4}\n{5}\t{6}\tL\t{7}\t{9}\n{5}\t{6}\tR\t{8}\t{10}\n".format("Strain", "Chromosome", "Contig_side", "Telom_length", "Offset", strain, chrom, left_tel, right_tel, offset, revoffset)) # file doesn't exist yet, write a header
 
 
-if __name__ == "__main__":
-    chr_start_end(sys.argv[1])
+# main program
+chr_start_end(sys.argv[1])

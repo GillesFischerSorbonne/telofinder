4f1028a46c81f455716f0a7e4c7022cb49c99852
Thomas Cokelaer <cokelaer@gmail.com>
2020-09-11 14:15:54 +0200

    add missing file

---
+++ telofinder/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+pkgname = "telofinder"
+
+try:
+    import easydev
+    telofinder_path = easydev.get_package_location(pkgname)
+    sharedir = os.sep.join([telofinder_path , pkgname, "data"])
+    def get_data(filename):
+        filename = sharedir + os.sep + filename
+        if os.path.exists(filename):
+            return filename
+        else:
+            raise IOError("unknown file {}".format(filename))
+except:
+    print('please install easydev with the command "pip install easydev"')
+
+
+

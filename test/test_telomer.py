f16afa2d2af01ad05a8c074b4d91cfd8ca9a60bf
Etienne Kornobis <ekornobis@gmail.com>
2023-10-20 16:04:22 +0200

    Update gh actions and test

--- test/test_telomer.py
+++ test/test_telomer.py
@@ -6,4 +6,4 @@ filename = f"{test_dir}/data/AFH_chrI.fasta"
 
 
 def test_run_on_single_fasta():
-    df = tf.run_on_single_fasta(filename, 0.8, 0.8, 8000, 1)
+    df = tf.run_on_single_fasta(filename, 0.8, 0.8, 8000, "both", 1)
